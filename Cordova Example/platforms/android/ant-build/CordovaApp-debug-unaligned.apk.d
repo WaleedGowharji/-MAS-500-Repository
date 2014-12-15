c82f09b35dc35ca647bab93be5aa17cd01ec3f2c
Waleed Gowharji <gowharji@mit.edu>
2014-12-15 16:15:17 -0500

    Homework 5
    homework

---
+++ Cordova Example/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/waleedgowharji/GitHub/GV-GetToKnow-Cordova/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/waleedgowharji/GitHub/GV-GetToKnow-Cordova/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/waleedgowharji/GitHub/GV-GetToKnow-Cordova/platforms/android/ant-build/classes.dex \
+/Users/waleedgowharji/GitHub/GV-GetToKnow-Cordova/platforms/android/CordovaLib/ant-build/classes.jar \
