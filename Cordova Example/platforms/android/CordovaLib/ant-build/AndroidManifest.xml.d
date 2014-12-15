da866e7c0ba80d8c139e03352b0141ff36c4adb4
Waleed Gowharji <gowharji@mit.edu>
2014-12-15 16:17:06 -0500

    Updates
    updates for assignment 5

---
+++ Cordova Example/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/waleedgowharji/GitHub/GV-GetToKnow-Cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/waleedgowharji/GitHub/GV-GetToKnow-Cordova/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ Cordova Example/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/waleedgowharji/GitHub/GV-GetToKnow-Cordova/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/waleedgowharji/GitHub/GV-GetToKnow-Cordova/platforms/android/AndroidManifest.xml \
